2be18a1bc36d326782bd21506e0c535411bb4f28
Abhishek Agrawal <25abhishekagrawal@gmail.com>
2026-04-05 23:15:04 +0530

    Created Process Component

---
+++ src/components/Process/Process.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Process = () => {
+  return (
+    <section>
+        <div className='mx-w-[1440px] px-20 mx-auto py-20'>
+            <div className='mr-auto w-fit'>
+                <Heading highlight = "Our" heading = "Process" />    
+            </div> 
+        </div>
+    </section>
+  )
+}
+
+export default Process
